b6f20a5ccf4205a414fdfa027067d33a72bc3119
Abhishek-DVD <abhishekdvd404@gmail.com>
2025-03-12 10:57:35 -0700

    bug-fix

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-    origin:["http://localhost:5173","https://abhishekballi13.github.io","https://personal-portfolio-abhishek-devs-projects-5f1be1ee.vercel.app"],
+    origin:["http://localhost:5173","https://abhishekballi13.github.io","https://personal-portfolio-alpha-three-24.vercel.app"],
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type","Authorization", "X-CSRF-Token", "X-Requested-With", "Accept", "Accept-Version", "Content-Length", "Content-MD5","Date", "X-Api-Version"],
